b4c76df15948acd597978e3c978fbc0429f184d7
trox <122801000+tr0x1337@users.noreply.github.com>
2025-10-02 22:56:20 +0200

    Add files via upload

---
+++ WhilePrimer1.java
@@ -0,0 +1,12 @@
+package whileprimer1;
+
+public class WhilePrimer1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 5){
+        System.out.println("Broj: " + i );
+        ++i;
+    
+        }
+    }
+}
