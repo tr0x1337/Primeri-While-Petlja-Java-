5c3d1c06793e0e341c0309e345191ece2bcdbeed
trox <122801000+tr0x1337@users.noreply.github.com>
2025-10-03 09:29:50 +0200

    Update WhilePrimer3.java

--- WhilePrimer3.java
+++ WhilePrimer3.java
@@ -6,39 +6,46 @@ public class WhilePrimer3 {
     public static void main(String[] args) {
         
         Scanner s = new Scanner(System.in);
+
+        String ponoviRacunanje = "da";
+        while(ponoviRacunanje.equalsIgnoreCase("da")){
         
-        System.out.print("Unesite od kog broja zelite da zapocnete sabiranje do " + "(i): ");
-        int i = s.nextInt();
-        
-        System.out.print("Broj od kog pocinje sabiranje je " + i + " \nUnesite broj do kog zelite da se vrsi sabiranje (n): ");
-        int n = s.nextInt();
-        
-        System.out.println("Uneli ste broj " + n + ".\n");
-        
-        
-        int iStart = i;
-        int zbir = 0;
-        
-        
-        System.out.println("\n\tPocetna vrednost za zbir je " + zbir + ".\n");
-        String matematickiIzraz = "";
-        
-        
-        if(i > n) {
-            System.out.println("Greska!");
-            return;
-        }
-        
-        
-        while(i <= n){
-                System.out.println("Dodaje se " + i + " u zbir, trenutni zbir je " + (zbir + i) + ".");
-                zbir += i;
-                matematickiIzraz += i + (i < n ? " + " : " = ");
-                ++i;
+            System.out.print("Unesite od kog broja zelite da zapocnete sabiranje do " + "(i): ");
+            int i = s.nextInt();
+            
+            System.out.print("Broj od kog pocinje sabiranje je " + i + " \nUnesite broj do kog zelite da se vrsi sabiranje (n): ");
+            int n = s.nextInt();
+            
+            System.out.println("Uneli ste broj " + n + ".\n");
+            
+            
+            int iStart = i;
+            int zbir = 0;
+            
+            
+            System.out.println("\n\tPocetna vrednost za zbir je " + zbir + ".\n");
+            String matematickiIzraz = "";
+            
+            
+            if(i > n) {
+                System.out.println("Greska!");
+                return;
             }
-               
-        System.out.println("\nZbir brojeva od " + iStart + " do " + n + " je " + zbir + ".");
-        System.out.println("Izraz " + matematickiIzraz + zbir);
-    
+            
+            
+            while(i <= n){
+                    System.out.println("Dodaje se " + i + " u zbir, trenutni zbir je " + (zbir + i) + ".");
+                    zbir += i;
+                    matematickiIzraz += i + (i < n ? " + " : " = ");
+                    ++i;
+                }
+                   
+            System.out.println("\nZbir brojeva od " + iStart + " do " + n + " je " + zbir + ".");
+            System.out.println("Izraz " + matematickiIzraz + zbir);
+            
+            System.out.print("Da li zelite da ponovite racunanje (da/ne): ");
+            ponoviRacunanje = s.next();
+        }
     }
 }
+
